1815917626c5923d410357dedb735559f7cb5a9e
Danil-hedgehog <Danilvel1909@gmail.com>
2023-11-28 21:55:11 +0700

    Completed.

--- 001/main.py
+++ 001/main.py
@@ -1,8 +1,18 @@
 f =  "C:/Python3/python.exe"
+#-----0123456789012345678920
+#g = f[start:end]
 
-g1 = f["python.exe"]
-g2 = f["exe"]
-g3 = f["Python3"]
-g4 = f["C:/Python3"]
+g1 = f[11:]
+g2 = f[18:]
+g3 = f[3:10]
+g4 = f[:10]
 
-print(f)
+count = 0
+for ch in f: 
+    print(f"{count}\t{ch}")
+    count += 1
+
+print(f"Имя файла: {g1}")
+print(f"Расширение: {g2}")
+print(f"Имя каталога: {g3}")
+print(f"Полный путь к каталогу: {g4}")
